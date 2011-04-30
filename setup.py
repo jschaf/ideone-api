89cbcb25de02265b7ba676573bcafec2a630e2dd
Joe Schafer <joe@jschaf.com>
2011-04-30 04:12:33 -0400

    Try different approach for reading README for long_description.

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import os
 from setuptools import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
 
 version = __import__('ideone').__version__
 
@@ -17,11 +18,11 @@ setup(
     platforms = ["any"],
     keywords = "API, ideone, codepad",
     packages = ['ideone'],
-    long_description=read('README.rst'),
+    long_description=readme,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
     ],
-    install_requires=['suds',]
+    install_requires=['suds'],
 )
